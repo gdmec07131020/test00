fa7a5c89c7003d53e65e7f769de089021dc99eef
gdmec07131020 <1063355293@qq.com>
2015-05-23 19:08:52 +0800

    sdfssdsfsd

---
+++ app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+//wechat
+var wechat =require('wechat');
+var config={
+  token:'hxrtest',
+  appid:'wx638fe59286bc0bb4',
+  encodingAESKey:'qO4AMIpkSvgHRBoXqyEggFbSvjHgIZxJt1N5JAdQGyT'
+};
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.use(express.query());
+app.use('/wechat',wechat(config,function(req,res,next){
+  var message=req.weixin;
+  res.reply([
+  {
+    title:'一起玩！',
+    description:'这是一个简单的塔防游戏',
+    picurl:'',
+    url:""
+  }
+    ])
+  }
+}))
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
